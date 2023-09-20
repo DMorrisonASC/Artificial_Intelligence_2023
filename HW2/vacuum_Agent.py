4219e8246c584216765dc4bfc3948c1e5fd762da
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-09-19 21:24:31 -0400

    Update vacuum_Agent.py

--- HW2/vacuum_Agent.py
+++ HW2/vacuum_Agent.py
@@ -17,32 +17,63 @@ class Agent:
 class Environment:
     clean = 0
     dirty = 1
-    def __init__(self):
+    def __init__(self, rows, cols):
         #iterate through envy and populate randomly
-        self.envy = [[random.randint(0,1) for i in range(10)] for i in range(10)]
+        self.rows = 10
+        self.cols = 10
+        self.envy = [[random.randint(0,1) for i in range(self.rows+2)] for i in range(self.cols+2)]
+        
+        for x in range(self.rows+2):
+            self.envy[x][0] = 2
+            self.envy[x][self.rows+1] = 2
 
+        for y in range(self.cols+2):
+            self.envy[0][y] = 2
+            self.envy[self.cols+1][y] = 2
+
+            
     def getStatus(self, posRow, posCol):
         print(self.envy[posRow][posCol])
 
     def setStatus(self, posRow, posCol, status):
         self.envy[posRow][posCol] = status
         print(self.envy[posRow][posCol])
+
+    def getDirtySpot(self):
+        pass
+
     
     def printEnvy(self):
         for eachRow in self.envy:
             print(eachRow , "\n")
 
-x = Environment()
-x.printEnvy()
+
+# x = Environment(10,10)
+# x.getStatus(0,3)
+# x.printEnvy()
 
 class Agent:
     def __init__(self):
-        playGround = Environment()
-        initalPos = [random.randint(0,10)][random.randint(0,10)]
-        currentPos = [initalPos[0]][initalPos[1]]
+        self.playGround = Environment(10,10)
+        self.initalPosX = random.randint(0,10)
+        self.initalPosY = random.randint(0,10)
+        self.currentPosX = int(self.initalPosX)
+        self.currentPosY = int(self.initalPosY)
+        # playGround.printEnvy()
 
-    def move(self, steps):
-        pass
+    def move(self, steps, destination, currentPosX, currentPosY):
+        arrived = False
+
+        while arrived == False:
+            for x in destination[0]:
+                if currentPosX > destination[0]:
+                    steps -= 1
+                    currentPosX -= 1
+                elif currentPosX < destination[0]:
+                    pass
+
+            
+        
 
     def goLeft(self):
         if currentPos[0] > 0:
@@ -58,6 +89,12 @@ class Agent:
     def goDown(self): 
         if currentPos[1] < 10:
             currentPos[0] += 1
+
+    def printArea(self):
+        self.playGround.printEnvy()
+    
+rooma = Agent()
+rooma.printArea()
 
 # x.getStatus(0,0)
     #populateEnvy()
@@ -93,3 +130,5 @@ class Agent:
 	#-agent1: agent		maybe
 	#+populateEnvy()
 	#+updateEnvy(x,y,u)
+
+
