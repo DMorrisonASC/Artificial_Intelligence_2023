3c572c789b295d098bb3eaf260a8f6a385b777b5
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-09-28 18:39:27 -0400

    Add files via upload

---
+++ HW3/Queue.py
@@ -0,0 +1,13 @@
+# Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+# Date: 
+# Instructor: Professor Silveyra
+# Description: 
+# Errors:
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        queue = deque()
+
+
+
