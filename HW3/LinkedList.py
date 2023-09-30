966ac73c17746134e6ddb81b528c8377e3db3c80
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-09-29 20:35:29 -0400

    Update LinkedList.py

--- HW3/LinkedList.py
+++ HW3/LinkedList.py
@@ -1,128 +1,108 @@
 # Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
-# Date: 
+# Date: 9/26/2023
 # Instructor: Professor Silveyra
-# Description: 
-# Errors:
+# Description: Students will implement a simple adjacency list
+# Errors: 1) Can NOT execute `DFS()` and `BFS()` are the same time 
 
 from Node import Node
+from Stack import Stack
+from Queue import MyQueue
+from LinkedList import LinkedList
+from Heap import MinHeap
+# from Stack import DFS
 
-# Create a LinkedList class
-class LinkedList:
-    def __init__(self):
-        self.head = None
+class AdjList:
+    def __init__(self, num):
+        self.V = num
+        self.graph = [None] * self.V
 
-    # Adds a node at begin of the LinkedList
+        for x in range(self.V):
+            self.graph[x] = LinkedList()
 
-    def getHead(self):
-        return self.head
-
-    def insertAtBegin(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        else:
-            new_node.next = self.head
-            self.head = new_node
-
-    # Adds a node at any index
-    def insertAtIndex(self, data, index):
-        new_node = Node(data)
-        current_node = self.head
-        position = 0
-        if position == index:
-            self.insertAtBegin(data)
-        else:
-            while(current_node != None and position+1 != index):
-                position = position+1
-                current_node = current_node.next
-
-            if current_node != None:
-                new_node.next = current_node.next
-                current_node.next = new_node
-            else:
-                print("Index not present")
-
-    # Update node of a linked list at specified index
-    def updateNode(self, val, index):
-        current_node = self.head
-        position = 0
-        if position == index:
-            current_node.data = val
-        else:
-            while(current_node != None and position != index):
-                position = position+1
-                current_node = current_node.next
-
-            if current_node != None:
-                current_node.data = val
-            else:
-                print("Index not present")
-
-    # Method to remove first node of linked list
-    def remove_begin(self):
-        if(self.head == None):
-            return
-
-        self.head = self.head.next
+    # Add edges
+    def insert(self, index, d):
+        selectedLinkedList = self.graph[index]
+        selectedLinkedList.insertAtBegin(d)
 
 
-    # Method to remove at given index
-    def remove_at_index(self, index):
-        if self.head == None:
-            return
+    # print a single  vertice
+    def print_Vertice(self, index):
+        selectedLinkedList = self.graph[index]
+        print("Vertex " + str(index) + ":", end="")
+        selectedLinkedList.printList()
 
-        current_node = self.head
-        position = 0
-        if position == index:
-            self.remove_begin()
-        else:
-            while(current_node != None and position+1 != index):
-                position = position+1
-                current_node = current_node.next
 
-            if current_node != None:
-                current_node.next = current_node.next.next
-            else:
-                print("Index not present")
+    # Print the graph
+    def print_graph(self):
+        for index in range(self.V):
+            selectedLinkedList = self.graph[index]
+            print("Vertex " + str(index) + ":", end="")
+            selectedLinkedList.printList()
+            print()
 
-    # Method to remove a node from linked list
-    def remove_node(self, data):
-        current_node = self.head
+    def DFS(self, startP):
+        stack = Stack()
+        stack.push(startP)
+        visited = []
 
-        while (current_node != None):
+        while stack.is_empty() == False:
+            eachV = stack.pop()
+    
+            if eachV not in visited:
+                visited.append(eachV)
+                current_node = self.graph[eachV].getHead()
 
-            if (current_node.data == data):
-                self.remove_begin()
-                return
-            if (current_node.next != None):
-                print("Node does not exist")
-                return
+                while current_node:
+                    if current_node != None:
+                        stack.push(current_node.data)
+                        current_node = current_node.next
 
-            elif (current_node.next.data != data):
-                current_node = current_node.next
-            else:
-                print(data, " was successfully removed")
-                current_node.next = current_node.next.next
-                return
+        print("Path: ",end="")
+        for i in visited:
+            print(i, end=" -> ")
 
-        print("Node does not exist")
+    def BFS(self, startP):
+        queue = MyQueue(self.V)
+        queue.enque(startP)
+        visited = []
 
-    # Print the size of linked list
-    def sizeOfList(self):
-        size = 0
-        if(self.head):
-            current_node = self.head
-            while(current_node):
-                size = size+1
-                current_node = current_node.next
-            return size
-        else:
-            return 0
+        while queue.is_empty() == False:
+            eachV = queue.deque()
 
-    # print method for the linked list
-    def printList(self):
-        current_node = self.head
-        while(current_node):
-            print(" -> {}".format(current_node.data), end="")
-            current_node =  current_node.next
+            if eachV not in visited:
+                visited.append(eachV)
+                current_node = self.graph[eachV].getHead()
+                while current_node:
+                    queue.enque(current_node.data)
+                    current_node = current_node.next
+
+        print("Path: ",end="")
+        for i in visited:
+            print(i, end=" -> ")
+
+    def Dijkstras():
+        pass
+
+    def remove_connect(self, index, connection):
+        linkedL = self.graph[index]
+        print(type(linkedL))
+        linkedL.remove_node(int(connection))
+
+    
+
+if __name__ == "__main__":
+    V = 5
+
+    # Create graph and edges
+    Adjacency_List = AdjList(V)
+    Adjacency_List.insert(0, 1)
+    Adjacency_List.insert(0, 2)
+    Adjacency_List.insert(1, 0)    
+    Adjacency_List.insert(1, 1)
+    Adjacency_List.print_graph()
+    # Adjacency_List.print_Vertice(0)
+    Adjacency_List.remove_connect(0,2)
+    Adjacency_List.print_graph()
+
+    DFS_results = Adjacency_List.DFS(0)
+    BFS_results = Adjacency_List.BFS(0)
