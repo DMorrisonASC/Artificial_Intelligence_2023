5403ebf74a3f6b0987fa6bb30a74631653749fb0
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2023-11-07 22:23:17 -0500

    Update N_grams.py

--- HW6/N_grams.py
+++ HW6/N_grams.py
@@ -28,55 +28,52 @@ class N_grams:
                     pass
 
     def add_unigrams(self, word):
+        # Create a new uni-gram key in dictionary if it doesn't exist. If it does exist, increment by 1
         try:
             self.unigram[word] += 1
         except KeyError:
             self.unigram[word] = 1
 
     def add_bigrams(self, word1, word2):
+        # Create a new bi-gram key in dictionary if it doesn't exist. If it does exist, increment by 1
         combination = word1 + " " + word2
         try:
             self.bigram[combination] += 1
         except KeyError:
             self.bigram[combination] = 1 
 
-    
     def print_unigrams(self, word): 
         print('"{}" appears {} time(s)'.format(word, self.unigram.get(word, 0)))
         print('"{}" appears {} time(s) at the beginning of a sentence'.format(word, self.bigram.get("</s><s>" + " " + word, 0)))
         print('"{}" appears {} time(s) at the ending of a sentence'.format(word, self.bigram.get(word + " " + "</s><s>", 0)))
-        for key in self.bigram.keys():
+        
+        for key in self.bigram.keys(): # Find all keys that have `word` as a seperate word
             if word + " " in key or " " + word in key:
                 print('"{}" appears {} time(s)'.format(key, self.bigram.get(key, 0)))
 
     def print_bigrams(self, current_word, prev_word):
         combin = prev_word + " " + current_word
         print('"{}" occurs {} time(s)'.format(combin, self.bigram.get(combin, 0)))
-    
-        # prob = (self.unigram.get(current_word, 1 / len(self.unigram)) + self.unigram.get(prev_word, 1 / len(self.unigram))) / self.unigram.get(prev_word, 1 / len(self.unigram))
+        # Calculate probability of a bi-gram
         prob = (self.bigram.get(combin, 2 / len(self.unigram))) / self.unigram.get(prev_word, 1 / len(self.unigram))
-        print('Probability of {} is {:%}'.format(combin, prob))
+        print('Probability of {} is {}'.format(combin, prob))
         
         return prob
 
     def print_sentence_prob(self, sentence):
-        # for eachWord in range(sentence):
-        #     pass
         pattern = r'\.(?:\s|\\)' # a regex expression that finds any expression of a puncation mark followed by a space or new line
         sentence_convert = re.sub(pattern, " </s><s> ", sentence)
-        # print(sentence_convert) # Print converted line
 
         totalprob = 1
-
+        # Calculate probability of a bi-gram
         for index in range(len(sentence_convert.split())):
             pair = sentence_convert.split()[index - 1] + " " + sentence_convert.split()[index - 1]
             if index != 0: 
                 prob = (self.bigram.get(pair, 1 / len(self.bigram))) / self.unigram.get(index-1, 1 / len(self.unigram))
                 totalprob = totalprob * prob
         
-        print('Probability of "{}" is {:%}'.format(sentence, totalprob))
-
-
+        print('Probability of "{}" is {}'.format(sentence, totalprob))
+        
     # def replace_spec_chars(self, word):
     #     hex_code = 0
     #     for char in word:
@@ -86,8 +83,6 @@ class N_grams:
 if __name__ == '__main__':
     allGrams =  N_grams()
     allGrams.load_file("pg236.txt")
-    allGrams.print_unigrams("Theaaaa")
+    allGrams.print_unigrams("good")
     allGrams.print_bigrams("deal", "good")
     allGrams.print_sentence_prob("I love carts. I love plants.")
-
-
